20d580288a0fc6eeb9d9b1df53c07d4869f9714e
shubhamh <shubhamhawaldar2123@gmail.com>
2023-10-14 12:16:05 +0530

    Setup react app without using create-react-app

---
+++ Introduction_JSX_and_Babel/webpack.config.cjs
@@ -0,0 +1,40 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  target: 'web',
+  devServer: {
+    port: '5000',
+    static: {
+      directory: path.join(__dirname, 'public')
+},
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/, 
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env','@babel/preset-react'] },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
+};
